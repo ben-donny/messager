df9fa83855b56d1f1eb14532d4d0ab32e84a8c03
benedict idonije <bidonije@googlemail.com>
2018-11-14 05:10:56 +0000

    adding messager application to github

---
+++ src/main/java/com/messager/service/MessagerApplication.java
@@ -0,0 +1,12 @@
+package com.messager.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/messager/service/com/messager/service/model/Message.java
@@ -0,0 +1,60 @@
+package com.messager.service.com.messager.service.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+    private String message;
+    private Date created;
+    private String author;
+
+    public Message() {
+    }
+
+    public Message(long id, String message, Date created, String author) {
+        this.id = id;
+        this.message = message;
+        this.created = created;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
